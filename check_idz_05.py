cb37ca45921b237f7477e5eda7ace1910001b3e1
nawww83 <nawww83@gmail.com>
2019-12-28 00:50:36 +0700

    * В модуль проверки задания №05 добавлена процедура поиска такого отношения сигнал-шум на один бит Eb/N0, при котором обеспечивается заданная вероятность битовой ошибки на выходе декодера (n, k)-кода со скоростью кодирования R = k / n.

--- check_idz_05.py
+++ check_idz_05.py
@@ -71,6 +71,9 @@ assert(m_ <= m_max)
 assert(p_ <= p_max)
 assert(p_ >= p_min)
 
+print(f'Требуемая вероятность битовой ошибки P бит. дек.: {p_}')
+print(f'Вид модуляции m: {m_}: {modulations[m_]}')
+
 wsC = wb['Check']
 params = []
 for row in wsC.iter_rows(min_row = 1, max_col = 1, max_row = 6, values_only = True):
@@ -84,8 +87,8 @@ R_, EbN0_dB_ = params
 assert(R_ > 0.)
 assert(R_ <= 1.)
 
-print(f'Вид модуляции: {m_}: {modulations[m_]}')
-print(f'Требуемое отношение сигнал-шум на один бит: {EbN0_dB_}, дБ')
+#print(f'Скорость кодирования R: {R_}')
+#print(f'Отношение сигнал-шум на один бит Eb/N0: {EbN0_dB_}, дБ')
 
 EbN0_ = er.dB2pow(EbN0_dB_)
 M_ = bits_per_symbol[m_]
@@ -93,32 +96,49 @@ EsN0_ = M_ * EbN0_ * R_
 
 p_sym_err, p_bit_err = error_func(m_)(EsN0_, coherencies[m_])
 
-print(p_bit_err)
-print(p_)
-assert(p_bit_err >= p_)
-
-n = 31
-k = round(R_ * n)
+# Решение задачи оптимизации
+p_bit_dec = p_ # Целевая вероятность
+n = 127
+m = m_
+M = bits_per_symbol[m]
+R = 0.55
+k = int(round(R * n))
 qi = lc.resolve_hamming_constrain(n, k)
-
-diff = n - qi
-p_err = 0.
-if diff > qi:
-    for q in range(qi + 1):
-        p_err += lc.probability_bsc(q, n, p_bit_err)
-    p_err = 1. - p_err
-else:
-    for q in range(qi + 1, n + 1):
-        p_err += lc.probability_bsc(q, n, p_bit_err)
-
 d = 2 * qi + 1
-p_bit = p_err * d / n
+rel_error = 1.
+EbN0 = 0.1
+dE = 0.0006
+EsN0 = er.EsN0(EbN0, R, M)
+while rel_error > 0.01:
+    _, p_bit_err = error_func(m)(EsN0, coherencies[m])
+    _, p_bit_err_2 = error_func(m)(EsN0 + dE, coherencies[m])
+    diff = n - qi
+    p_err = 0.
+    p_err_2 = 0.
+    if diff > qi:
+        for q in range(qi + 1):
+            p_err += lc.probability_bsc(q, n, p_bit_err)
+            p_err_2 += lc.probability_bsc(q, n, p_bit_err_2)
+        p_err = 1. - p_err
+        p_err_2 = 1. - p_err_2
+    else:
+        for q in range(qi + 1, n + 1):
+            p_err += lc.probability_bsc(q, n, p_bit_err)
+            p_err_2 += lc.probability_bsc(q, n, p_bit_err_2)
 
-print(p_bit)
-print(p_)
-
-rel_error = np.abs(p_bit - p_) / p_
-assert(rel_error < 0.01)
+    p_bit = p_err * d / n
+    p_bit_2 = p_err_2 * d / n
+    rel_error = np.fabs(p_bit - p_bit_dec) / p_bit_dec
+    if p_bit_2 > p_bit:
+        EsN0 -= np.sign(p_bit - p_bit_dec)* dE
+    else:
+        EsN0 += np.sign(p_bit - p_bit_dec)* dE
+    # print(f'... EbN0: {er.pow2dB(er.EbN0(EsN0, R, M))} дБ, P бит. дек. {p_bit}')
+print(f'Скорость кодирования R: {R}')
+print(f'EbN0: {er.pow2dB(er.EbN0(EsN0, R, M)):.2f} дБ')
+#
+print(f'P бит. дек.:{p_bit}')
+print(f'Длина кода n: {n}')
 
 print('All Ok')
 
